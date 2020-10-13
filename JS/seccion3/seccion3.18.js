5ee5cfffd61a38ca358da993b571ec4b94cd4ffe
Sofia <sofiapri@ucm.es>
2020-10-13 15:51:47 +0200

    algunas secciones más

--- JS/seccion3/seccion3.18.js
+++ JS/seccion3/seccion3.18.js
@@ -2,3 +2,67 @@
 // *SECCION 3.18
 //*******************************
 
+/*
+HTML
+
+<html>
+
+<head>
+    <title>Sesion 3</title>
+    <script src="JS/keypress.js"></script>
+    <script src="JS/juego.js"></script>
+</head>
+
+<body onload = 'inicializar();'>
+    
+    <button onclick='vender();'>Vender</button>
+    <button onclick='muestraInventario();'>INVENTARIO</button>
+
+</body>
+</html>
+
+*/
+
+
+function inicializar(){
+
+
+}
+
+//var lista = ['Espada', 'Pocion', 'Daga', 'Armadura', 'Flecha', 'Cola de Fenix'];
+//console.log(lista.length);
+
+var lista = [];
+
+lista.push('A');
+lista.push('B');
+lista.push('C');
+lista.push('D');
+lista.push('E');
+
+/*function compra(objeto){
+    
+    if(objeto ==1){
+        lista.push('Espada');
+    }
+    else{
+        lista.push('Pocion');
+    }
+
+    muestraInventario();
+}*/
+
+
+function vender(){
+    //lista.pop();
+    //muestraInventario();
+    lista.splice(2,1);
+}
+
+
+function muestraInventario(){
+    for(a=0; a<lista.length; a++){
+        console.log(a+ '-'+lista[a]);
+    }
+}
+
